1f96ee8e84ef61a36e717f96888b4f0e2b05d48f
juanantonioruz <juanantonioruz@gmail.com>
2012-01-18 13:34:10 +0100

    desconectando sessiones

--- src/com/enjava/FiltroSes.java
+++ src/com/enjava/FiltroSes.java
@@ -15,53 +15,61 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
-
-public class FiltroSes implements Filter{
+public class FiltroSes implements Filter {
 
 	@Override
 	public void destroy() {
-		
+
 	}
 
 	@Override
 	public void doFilter(ServletRequest arg0, ServletResponse arg1,
 			FilterChain arg2) throws IOException, ServletException {
-		HttpServletRequest req=(HttpServletRequest) arg0;
+		HttpServletRequest req = (HttpServletRequest) arg0;
 		String host = req.getHeader("Host");
 		String userAgent = req.getHeader("User-Agent");
-		String id=host+"-"+userAgent;
+		String id = host + "-" + userAgent;
 		System.out.println(id);
 		HttpSession session = req.getSession();
 		ServletContext servletContext = session.getServletContext();
-		HashMap<String, Map> usuarios = (HashMap<String, Map>) servletContext.getAttribute("usuarios");
-		if(usuarios.containsKey(id)){
+		HashMap<String, Map> usuarios = (HashMap<String, Map>) servletContext
+				.getAttribute("usuarios");
+		if (usuarios.containsKey(id)) {
 			Map<String, Object> map = usuarios.get(id);
-			for(String key:map.keySet()){
+			for (String key : map.keySet()) {
 				session.setAttribute(key, map.get(key));
 			}
-		}else{
+		} else {
 			usuarios.put(id, new HashMap());
 		}
 		arg2.doFilter(arg0, arg1);
-		Map<String,Object> m=usuarios.get(id);
-		Enumeration names=session.getAttributeNames();
-		while(names.hasMoreElements()){
-			String keySession = (String) names.nextElement();
-			Object valueSession = session.getAttribute(keySession);
-			m.put(keySession, valueSession);
-			System.out.println(valueSession);
+
+		Map<String, Object> m = usuarios.get(id);
+		try {
+
+			Enumeration names = session.getAttributeNames();
+			while (names.hasMoreElements()) {
+				String keySession = (String) names.nextElement();
+				Object valueSession = session.getAttribute(keySession);
+				m.put(keySession, valueSession);
+				System.out.println(valueSession);
+			}
+		} catch (IllegalStateException e) {
+			System.out.println(usuarios.size());
+			usuarios.remove(id);
+			System.out.println(usuarios.size());
 		}
-		
-//		Enumeration headerNames = req.getHeaderNames();
-//		while(headerNames.hasMoreElements()){
-//			String header = (String) headerNames.nextElement();
-//			System.out.println("header: "+header+":"+req.getHeader(header));
-//		}
+
+		// Enumeration headerNames = req.getHeaderNames();
+		// while(headerNames.hasMoreElements()){
+		// String header = (String) headerNames.nextElement();
+		// System.out.println("header: "+header+":"+req.getHeader(header));
+		// }
 	}
 
 	@Override
 	public void init(FilterConfig arg0) throws ServletException {
-		
+
 	}
 
 }
